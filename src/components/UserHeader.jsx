098194f4cdf601ab90ced1b3b59a8eadeda30bc4
Lenny Peters <mr.lennypeters@gmail.com>
2020-08-08 08:39:18 -0700

    refactor: 💡 UserHeader.js
    refactored into hook instead

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import { shape, string } from "prop-types";
 import { connect } from "react-redux";
 
-// Would love to see hooks handle this instead.. TODO: LKP
-// eslint-disable-next-line react/prefer-stateless-function
-class UserHeader extends Component {
-  render() {
-    const { user } = this.props;
-    if (!user) {
-      return null;
-    }
-    return <div className="header">{user.name}</div>;
+const UserHeader = ({ user }) => {
+  if (!user) {
+    return null;
   }
-}
+  return <div className="header">{user.name}</div>;
+};
 
 UserHeader.defaultProps = {
   user: {
